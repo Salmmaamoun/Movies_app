6c509210e364c68f36884e8f0aa4b852405a880e
Salmmaamoun <allamsalma299@gmail.com>
2023-11-14 07:11:42 +0200

    update ApiService

--- src/main/java/com/example/movies/data/api/ApiService.kt
+++ src/main/java/com/example/movies/data/api/ApiService.kt
@@ -1,6 +1,4 @@
 package com.example.movies.data.api
-import com.example.movies.data.api.model.MovieDetailsResponse
-import com.example.movies.data.api.model.PopularMovieResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
